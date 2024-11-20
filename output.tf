f42e0628d8a9880461565520c1c6491ae972a103
Anuradha Wickramarachchi <anuradhawick@gmail.com>
2024-11-20 09:00:47 +0000

    DEV: add TF outputs

---
+++ output.tf
@@ -0,0 +1,36 @@
+# sbeacon
+output "api_url" {
+  value       = module.sbeacon.api_url
+  description = "URL used to invoke the API."
+}
+
+output "api_stage" {
+  value       = module.sbeacon.api_stage
+  description = "API stage."
+}
+
+# cognito
+output "cognito_client_id" {
+  value       = module.cognito.cognito_client_id
+  description = "Cognito client Id for user registration and login."
+}
+
+output "cognito_user_pool_id" {
+  value       = module.cognito.cognito_user_pool_id
+  description = "Cognito user pool Id."
+}
+
+output "admin_login_command" {
+  value       = module.cognito.admin_login_command
+  description = "Admin Login Command"
+}
+output "guest_login_command" {
+  value       = module.cognito.guest_login_command
+  description = "Guest Login Command"
+}
+
+# webgui
+output "beacon_ui_url" {
+  value       = module.webgui.cloudfront-url
+  description = "URL of the webapp."
+}
