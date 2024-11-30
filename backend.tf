e202904ce1e202eb3045687ea0259996ee09f3f1
Dwi Fahni Denni <dfdenni0912@gmail.com>
2024-11-30 09:18:12 +0700

    Added backend & terraform.tfvars example

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-states-testing-deployments"
-    key            = "gaspi-infrastructure-deployment"
-    region         = "ap-southeast-2"
-    dynamodb_table = "terraform-states-testing-deployments"
-  }
-}
