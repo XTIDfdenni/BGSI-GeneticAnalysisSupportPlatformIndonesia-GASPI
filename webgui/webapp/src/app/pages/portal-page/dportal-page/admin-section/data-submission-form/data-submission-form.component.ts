6ba252eb4f953b15a2abb0a2fdad11663991e58a
NickEdwards7502 <nickedwards7502@gmail.com>
2025-01-10 03:58:46 +0000

    FIX: Correct staging prefix for file upload on project creation
    * Sync with sbeacon upstream

--- webgui/webapp/src/app/pages/portal-page/dportal-page/admin-section/data-submission-form/data-submission-form.component.ts
+++ webgui/webapp/src/app/pages/portal-page/dportal-page/admin-section/data-submission-form/data-submission-form.component.ts
@@ -63,7 +63,7 @@ export class DataSubmissionFormComponent {
   async uploadFile(path: string, file: File): Promise<string> {
     this.fileProgress.set(file.name, 0);
     try {
-      await Storage.put(`/staging/projects/${path}/${file.name}`, file, {
+      await Storage.put(`staging/projects/${path}/${file.name}`, file, {
         customPrefix: { public: '' },
         progressCallback: (progress: { loaded: number; total: number }) => {
           this.fileProgress.set(file.name, progress.loaded);
