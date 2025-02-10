6d12e8af92043cc454d8f83d9b4da3ce12c814ee
coy102 <fajarsep12@gmail.com>
2025-02-10 12:19:10 +0700

    feat: pagination svep

--- webgui/webapp/src/app/pages/clinic-page/svep-submit/projects-list/projects-list.component.ts
+++ webgui/webapp/src/app/pages/clinic-page/svep-submit/projects-list/projects-list.component.ts
@@ -1,12 +1,26 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  Output,
+  EventEmitter,
+  ViewChild,
+  Injectable,
+  ChangeDetectorRef,
+} from '@angular/core';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { catchError, of } from 'rxjs';
+import { catchError, of, Subject } from 'rxjs';
 import { MatIconModule } from '@angular/material/icon';
 import { MatRadioModule } from '@angular/material/radio';
 import { DportalService } from 'src/app/services/dportal.service';
 import { ComponentSpinnerComponent } from 'src/app/components/component-spinner/component-spinner.component';
+import {
+  MatPaginator,
+  MatPaginatorIntl,
+  MatPaginatorModule,
+  PageEvent,
+} from '@angular/material/paginator';
+import { isEmpty } from 'lodash';
 
 interface ProjectFile {
   filename: string;
@@ -25,14 +39,36 @@ export interface FileSelectEvent {
   vcf: string;
 }
 
+@Injectable()
+export class MyCustomPaginatorIntl implements MatPaginatorIntl {
+  changes = new Subject<void>();
+
+  // For internationalization, the `$localize` function from
+  // the `@angular/localize` package can be used.
+  firstPageLabel = $localize`First page`;
+  itemsPerPageLabel = $localize`Items per page:`;
+  lastPageLabel = $localize`Last page`;
+
+  // You can set labels to an arbitrary string too, or dynamically compute
+  // it through other third-party internationalization libraries.
+  nextPageLabel = 'Next page';
+  previousPageLabel = 'Previous page';
+
+  getRangeLabel(page: number, pageSize: number, length: number): string {
+    return $localize`Page ${page + 1}`;
+  }
+}
+
 @Component({
   selector: 'app-projects-list',
+  providers: [{ provide: MatPaginatorIntl, useClass: MyCustomPaginatorIntl }],
   standalone: true,
   imports: [
     MatTableModule,
     MatButtonModule,
     MatIconModule,
     MatRadioModule,
+    MatPaginatorModule,
     ComponentSpinnerComponent,
   ],
   templateUrl: './projects-list.component.html',
@@ -53,29 +89,55 @@ export class ProjectsListComponent {
   projectName: string | null = null;
   vcfFile: string | null = null;
 
+  protected pageSize = 5;
+  @ViewChild('paginator')
+  paginator!: MatPaginator;
+  private pageTokens = new Map<number, string>();
+  private isEmptyLastPage = false;
+
   constructor(
     private dps: DportalService,
     private sb: MatSnackBar,
-  ) {
-    this.list();
+    private cd: ChangeDetectorRef,
+  ) {}
+
+  ngOnInit(): void {
+    this.list(0);
+    this.cd.detectChanges();
+
+    this.paginator.page.subscribe((event: PageEvent) => {
+      if (this.pageSize != this.paginator.pageSize) {
+        this.resetPagination();
+        this.refresh();
+      } else {
+        this.list(event.pageIndex);
+      }
+    });
   }
 
-  list() {
+  list(page: number) {
     this.loading = true;
     this.dps
-      .getMyProjects()
+      .getMyProjects(this.pageSize, this.pageTokens.get(page))
       .pipe(
         catchError((error) => {
           console.error('Error fetching projects:', error); // Log the error
           return of(null); // Return null to continue the stream
         }),
       )
-      .subscribe(({ data }: any) => {
-        if (!data) {
+      .subscribe((response: any) => {
+        if (!response.data) {
           this.sb.open('API request failed', 'Okay', { duration: 60000 });
           this.dataSource.data = [];
         } else {
-          this.dataSource.data = data.map((project: any) => {
+          if (this.isEmptyLastPage && this.paginator.pageIndex > 0) {
+            this.paginator.pageIndex--;
+            this.sb.open('No more items to show', 'Okay', { duration: 60000 });
+            this.loading = false;
+            return;
+          }
+
+          this.dataSource.data = response.data.map((project: any) => {
             const vcfFiles = project.files.filter(
               (file: string) =>
                 file.endsWith('.vcf.gz') || file.endsWith('.bcf.gz'),
@@ -97,9 +159,28 @@ export class ProjectsListComponent {
               indexed: false,
             };
           });
+
+          // set next page token
+          this.pageTokens.set(page + 1, response.last_evaluated_key);
+          this.isEmptyLastPage = isEmpty(response.last_evaluated_key);
         }
         this.loading = false;
       });
+  }
+
+  resetPagination() {
+    this.pageTokens = new Map<number, string>();
+    this.paginator.pageIndex = 0;
+    this.pageSize = this.paginator.pageSize;
+  }
+
+  refresh() {
+    try {
+      this.resetPagination();
+      this.list(0);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   isFileSelected(fileName: string, projectName: string): boolean {
