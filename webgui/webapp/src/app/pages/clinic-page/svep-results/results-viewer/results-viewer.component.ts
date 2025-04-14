c70b7bac435a7386b7ecc1728750127dc18530a0
Brendan Hosking <brendan.hosking@gmail.com>
2025-04-14 07:00:36 +0000

    FEAT: Remove unused "." column from clinic results

--- webgui/webapp/src/app/pages/clinic-page/svep-results/results-viewer/results-viewer.component.ts
+++ webgui/webapp/src/app/pages/clinic-page/svep-results/results-viewer/results-viewer.component.ts
@@ -117,7 +117,6 @@ export class ResultsViewerComponent implements OnChanges, AfterViewInit {
   protected columns: string[] = [
     'selected',
     'Rank',
-    '.',
     'Region',
     'Alt Allele',
     'Consequence',
