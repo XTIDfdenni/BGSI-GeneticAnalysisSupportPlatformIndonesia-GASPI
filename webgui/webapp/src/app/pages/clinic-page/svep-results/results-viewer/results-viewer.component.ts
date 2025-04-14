beef352c4318cccc54e1bf81d2b9cda29dff785c
Brendan Hosking <brendan.hosking@gmail.com>
2025-04-14 07:18:13 +0000

    FEAT: Add additional columns to clinic results
    Columns are available in the backend, but not in the frontend.
    
    Add placeholder columns to the frontend so the full set of columns can
    be viewed.
    TODO: Give these columns nicer names and descriptions (in
    help-text.component.html) and reorder them (in svep/main.tf).

--- webgui/webapp/src/app/pages/clinic-page/svep-results/results-viewer/results-viewer.component.ts
+++ webgui/webapp/src/app/pages/clinic-page/svep-results/results-viewer/results-viewer.component.ts
@@ -130,6 +130,20 @@ export class ResultsViewerComponent implements OnChanges, AfterViewInit {
     'Codon Change',
     'Strand',
     'Transcript Support Level',
+    'ref',
+    'gt',
+    'qual',
+    'filter',
+    'variationId',
+    'rsId',
+    'omimId',
+    'classification',
+    'conditions',
+    'clinSig',
+    'reviewStatus',
+    'lastEvaluated',
+    'accession',
+    'pubmed',
   ];
   protected originalRows: any[] = [];
   protected dataRows = new BehaviorSubject<any[]>([]);
