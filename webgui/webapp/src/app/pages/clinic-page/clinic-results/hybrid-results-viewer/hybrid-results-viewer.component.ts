59401a5fa68d54ce106901b5f64f49c34bb57852
NickEdwards7502 <nickedwards7502@gmail.com>
2025-07-30 01:56:05 +0000

    STYLE: Wrap hybrid results tabs in mat-card for improved layout

--- webgui/webapp/src/app/pages/clinic-page/clinic-results/hybrid-results-viewer/hybrid-results-viewer.component.ts
+++ webgui/webapp/src/app/pages/clinic-page/clinic-results/hybrid-results-viewer/hybrid-results-viewer.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
 import { MatTabsModule } from '@angular/material/tabs';
 import { PharmcatResultsViewerComponent } from '../pharmcat-results-viewer/pharmcat-results-viewer.component';
 import { LookupResultsViewerComponent } from '../lookup-results-viewer/lookup-results-viewer.component'; // adjust path as needed
@@ -9,6 +10,7 @@ import { LookupResultsViewerComponent } from '../lookup-results-viewer/lookup-re
   standalone: true,
   imports: [
     CommonModule,
+    MatCardModule,
     MatTabsModule,
     PharmcatResultsViewerComponent,
     LookupResultsViewerComponent,
